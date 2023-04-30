b2ee235d9bfe616c483f87b649bab67495c7f795
Alessandro Lorenzi <alessandro.lorenzi@casavo.com>
2023-04-30 09:30:05 +0200

    chore: testing dynamodb for rating

--- tests/test_rating.py
+++ tests/test_rating.py
@@ -1,76 +1,76 @@
 import boto3
 
-class TestRating:
-    def test_initialize_rating(self):
-        boto3.setup_default_session(profile_name='alorenzi', region_name='eu-south-1')
-        dynamodb_client = boto3.client('dynamodb') 
-        response = dynamodb_client.create_table(
-            TableName='alorenzi-pictures-ratings',
-            KeySchema=[
-                {
-                    'AttributeName': 'photo_name',
-                    'KeyType': 'HASH',
-                },
-                {
-                    'AttributeName': 'rating',
-                    'KeyType': 'RANGE',
-                },
-            ],
-            AttributeDefinitions=[
-                {
-                    'AttributeName': 'photo_name',
-                    'AttributeType': 'S',
-                },
-                {
-                    'AttributeName': 'rating',
-                    'AttributeType': 'N',
-                },
-            ],
-            ProvisionedThroughput={
-                'ReadCapacityUnits': 1,
-                'WriteCapacityUnits': 1,
-            },
-        )
-        assert response['ResponseMetadata']['HTTPStatusCode'] == 200
+TABLE_NAME='alorenzi-pictures-test'
+
+class Rating:
+    def __init__(self, dynamodb_client, table_name):
+        self.dynamodb_client = dynamodb_client
+        self.table_name = table_name
     
-    def test_set_rating(self):
-        boto3.setup_default_session(profile_name='alorenzi', region_name='eu-south-1')
-        dynamodb_client = boto3.client('dynamodb')
-        response = dynamodb_client.put_item(
-            TableName='alorenzi-pictures-ratings',
+    def set_rating(self, photo_name, rating):
+        self.dynamodb_client.put_item(
+            TableName=self.table_name,
             Item={
                 'photo_name': {
-                    'S': '2023/04/23/DSC06732.JPG',
+                    'S': photo_name,
                 },
                 'rating': {
-                    'N': '5',
+                    'N': str(rating),
                 },
             },
         )
-        assert response['ResponseMetadata']['HTTPStatusCode'] == 200
     
-    def test_get_rating(self):
-        boto3.setup_default_session(profile_name='alorenzi', region_name='eu-south-1')
-        dynamodb_client = boto3.client('dynamodb')
-        response = dynamodb_client.get_item(
-            TableName='alorenzi-pictures-ratings',
+    def get_rating(self, photo_name):
+        response = self.dynamodb_client.get_item(
+            TableName=self.table_name,
             Key={
                 'photo_name': {
-                    'S': '2023/04/23/DSC06732.JPG',
+                    'S': photo_name,
                 },
             },
         )
-        assert response['Item']['rating']['N'] == '5'
+        if 'Item' not in response:
+            return 0
+        
+        return int(response['Item']['rating']['N'])
+    
+    def get_photos_by_rating(self, rating):
+        response = self.dynamodb_client.scan(
+            TableName=TABLE_NAME,
+            ScanFilter={
+                'rating': {
+                    'AttributeValueList': [
+                        {
+                            'N': '4',
+                        },
+                    ],
+                    'ComparisonOperator': 'GE',
+                },
+            },
+        )
+        return response['Items']
+
+class TestRating:
+    def setup_method(self):
+        boto3.setup_default_session(profile_name='alorenzi', region_name='eu-south-1')
+        self.dynamodb_client = boto3.client('dynamodb')
+        self.rating = Rating(self.dynamodb_client, TABLE_NAME)
+    
+    def test_set_rating(self):
+        self.rating.set_rating('test.jpg', 5)
+        assert self.rating.get_rating('test.jpg') == 5
+
+    
+    def test_get_rating(self):
+        assert self.rating.get_rating('test.jpg') == 5
 
     def test_get_all_5_stars(self):
-        boto3.setup_default_session(profile_name='alorenzi', region_name='eu-south-1')
-        dynamodb_client = boto3.client('dynamodb')
-        response = dynamodb_client.get_item(
-            TableName='alorenzi-pictures-ratings',
-            Key={
-                'rating': {
-                    'N': '5',
-                },
-            },
-        )
-        assert response['Item'] == []
+        assert len(self.rating.get_photos_by_rating(5)) == 1
+
+    def test_get_all_4_plus_stars(self):
+        self.rating.set_rating('test_4.jpg', 4)
+        self.rating.set_rating('test_3.jpg', 3)
+        assert len(self.rating.get_photos_by_rating(4)) == 2
+
+    def test_get_rating_not_found(self):
+        assert self.rating.get_rating('FOO.JPG') == 0
